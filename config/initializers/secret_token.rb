7ad872024f971498740971225acd45d4397cf77e
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-01-05 09:22:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BathroomRater::Application.config.secret_token = '483d8c207fb13825735d4ccdae32214e4f0e1b57fed15c283412b27de251aecb2001559dd18840c911c5d54260b28eb91fbfa3b857345eaf9f0565de61b02d5b'
