7d360b99a5b14d25d35f3d497c85a67da577ba22
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-01-05 14:36:08 -0500

    fix float bug

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -14,9 +14,9 @@ class CheckinsController < ApplicationController
           total = total + score.value  
         end
         
-        average_rating = total/scores.count
+        average_rating = total/(scores.count.to_f)
           
-        reply_text = "There is one bathroom here, it has an average rating of #{average_rating.to_f} out of 5 stars. Add your own rating!"
+        reply_text = "There is one bathroom here, it has an average rating of #{average_rating.round(2)} out of 5 stars. Add your own rating!"
       elsif b_count > 1
         total = 0
         btotal = 0
@@ -26,12 +26,12 @@ class CheckinsController < ApplicationController
             scores.each do |score|
               total = total + score.value  
             end
-          btotal = btotal + (total/scores.count).to_f  
+          btotal = btotal + (total/(scores.count.to_f))  
         end  
         
-        average_b_rating = (btotal/b_count).to_f
+        average_b_rating = (btotal/(b_count.to_f))
         
-        reply_text ="There is #{b_count} bathrooms here with an average rating of #{average_b_rating.to_f} out of 5 stars. Check out the details!" 
+        reply_text ="There is #{b_count} bathrooms here with an average rating of #{average_b_rating.round(2)} out of 5 stars. Check out the details!" 
       else
         reply_text ="Mayday! There are no bathrooms recorded here. Add one!"
       end      
